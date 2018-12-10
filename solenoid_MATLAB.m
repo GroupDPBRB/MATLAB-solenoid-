25523addaf23f8c913baa9bfc361ba3049cbeebe
GroupDPBRB <44881184+GroupDPBRB@users.noreply.github.com>
2018-12-10 11:20:00 -0800

    Update solenoid_MATLAB.m

--- solenoid_MATLAB.m
+++ solenoid_MATLAB.m
@@ -27,15 +27,15 @@ disp('Serial Port is Open')                                 % Display text to us
 while true                                                  % Create while statement for true
     % Ask user for input as a string
     user_input = input('Type "D" to open the release mechanism , "C" to close the release mechanism or "q" to quit : ','s');
-    if strcmp(user_input,'D')                               % If statement with strcmp() logicial statement
+    if strcmp(user_input,'D')                               % If statement with strcmp() logical statement
         send_str = 'D';                                     % Creates send_str variable as a string 'D'
         fprintf(a, '%s', send_str);                         % fprintf() to send signal to Arduino software and display string text
         pause(0.01);
-    elseif strcmp(user_input,'C')                           % Elseif statement with strcmp() logicial statement
+    elseif strcmp(user_input,'C')                           % Elseif statement with strcmp() logical statement
         send_str = 'C';                                     % Creates send_str variable as a string 'C'
         fprintf(a, '%s', send_str);                         % fprintf() to send signal to Arduino software and display string text
         pause(0.01);
-    elseif strcmp(user_input,'q')                           % Elseif statement with strcmp() logicial statement
+    elseif strcmp(user_input,'q')                           % Elseif statement with strcmp() logical statement
         break                                               % Terminates if statement
     end                                                     % Ends if statement
 end                                                         % Ends while statement
